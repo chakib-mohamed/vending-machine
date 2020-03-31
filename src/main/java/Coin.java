45498cd8ac07be6c802330ade48aed0b21f97329
chakibovski <chakib.mohamed2@gmail.com>
2020-03-31 19:01:16 +0100

    Change coins order

--- src/main/java/Coin.java
+++ src/main/java/Coin.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public enum Coin {
 
-    TWENTY_CENT(20d), FIFTY_CENT(50d), TEN_CENT(10d);
+    FIFTY_CENT(50d), TWENTY_CENT(20d) , TEN_CENT(10d);
 
     @Getter
     @Setter
